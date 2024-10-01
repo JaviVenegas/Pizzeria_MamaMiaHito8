b7308b0e8534454bfa15fd7d6a894550c3e9db11
Javiera Venegas <javieravenegas.arch@gmail.com>
2024-10-01 12:26:42 -0300

    boton

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -1,8 +1,16 @@
 import { useContext, useEffect } from 'react';
 import { UserContext } from '../contexts/UserContext';
+import { useNavigate } from 'react-router-dom';
+
 
 const Profile = () => {
     const { getUser, user} = useContext(UserContext);
+    const navigate = useNavigate();
+
+    const logout = () => {
+      navigate("/")
+      setUser ({ username: "", password: "", token: false })
+  }
   
     useEffect(() => {
       getUser();
@@ -20,6 +28,9 @@ const Profile = () => {
           ) : (
             <p>Please login to view your profile.</p>
           )}
+        </div>
+        <div className="buttons">
+          <button onClick={() => (logout())} className="btn btn-primary px-4">Logout</button>
         </div>
       </div>
     );
